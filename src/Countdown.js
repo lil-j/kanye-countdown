0131e05b0237af3de89d5a002d37b6999ebe10d2
jake <jke.hrpr@gmail.com>
2019-10-24 10:47:57 -0700

    updated timezone

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,20 +1,30 @@
 import React from 'react';
 
 class Counter extends React.Component {
+
     constructor(props) {
         super(props);
         // Get today's date and time
         var countDownDate = new Date("Oct 24, 2019 19:00:00").getTime();
+        var timeZoneOffset = new Date().getTimezoneOffset() - (countDownDate);
         var now = new Date().getTime();
         // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        this.state = {date: hours + "h "
-        + minutes + "m " + seconds + "s "};
+        var distance = countDownDate - now - timeZoneOffset;
+        if (distance < 0) {
+            this.state = {
+                date: 'ALBUM *SHOULD* BE OUT!'
+            };
+        } else{
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            this.state = {
+                date: hours + "h "
+                + minutes + "m " + seconds + "s"
+            };
+        }
     }
     componentDidMount() {
         this.timerID = setInterval(
@@ -28,18 +38,27 @@ class Counter extends React.Component {
     tick() {
         // Get today's date and time
         var countDownDate = new Date("Oct 24, 2019 19:00:00").getTime();
+
+        var timeZoneOffset = new Date().getTimezoneOffset() - (countDownDate);
         var now = new Date().getTime();
         // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        this.setState({
-            date: hours + "h "
-            + minutes + "m " + seconds + "s"
-        });
+        var distance = countDownDate - now - timeZoneOffset;
+        if (distance < 0) {
+            this.setState({
+                date: 'ALBUM *SHOULD* BE OUT!'
+            });
+        } else{
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            this.setState({
+                date: hours + "h "
+                + minutes + "m " + seconds + "s"
+            });
+        }
+        
     }
     render() {
         return (
