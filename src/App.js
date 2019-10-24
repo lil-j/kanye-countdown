9bf147246bb650aabf61854c1bc00952e8784229
jake <jke.hrpr@gmail.com>
2019-10-24 08:17:42 -0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Counter from './Countdown'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter className="counter"/>
+        <img style={{'z-index':4}} src="https://pbs.twimg.com/media/EHSh4xMXUAAMbrn?format=jpg&name=medium"></img>
       </header>
+      <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=eb56e24ef84504498ecdb102b3f0da6b26b9d918'></script>
+      <script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/590983/t/3"></script>
     </div>
   );
 }
---
+++ src/Countdown.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        // Get today's date and time
+        var countDownDate = new Date("Oct 24, 2019 19:00:00").getTime();
+        var now = new Date().getTime();
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        this.state = {date: hours + "h "
+        + minutes + "m " + seconds + "s "};
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    tick() {
+        // Get today's date and time
+        var countDownDate = new Date("Oct 24, 2019 19:00:00").getTime();
+        var now = new Date().getTime();
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        this.setState({
+            date: hours + "h "
+            + minutes + "m " + seconds + "s"
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h4 style={{'font-family':'VCR'}}>KANYE WEST'S</h4>
+                <h3 style={{'font-family':'NOVA','font-size':'380%'}}>JESUS IS KING</h3>
+                <h1 style={{'font-family':'VCR'}}>{this.state.date}</h1>
+            </div>
+        );
+        }
+    }
+
+export default Counter;
